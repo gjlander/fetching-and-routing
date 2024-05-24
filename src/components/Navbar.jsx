af531fe4dc34cb6cc458d9837f5840692f4af376
Garrett Lander <garrett.jlander@gmail.com>
2024-05-24 12:15:05 +0200

    added some clarifying comments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className='nav'>
+            {/* NavLink adds class "active" to the one where the "to" value matches the current URL, you can then use that class to add your own styling */}
             <NavLink to={'/'}>Home</NavLink>
             <NavLink to={'store'}>Store</NavLink>
         </nav>
